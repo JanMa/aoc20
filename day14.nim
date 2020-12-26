de48236b4fe720f3b5c3bb037357082aafe245c6
Jan Martens <jan.martens@rewe-digital.com>
2020-12-26 23:18:19 +0100

    day fourteen - part two

--- day14.nim
+++ day14.nim
@@ -1,4 +1,4 @@
-import math, strutils, tables
+import math, strutils, tables, sequtils
 
 type
   Bit = range[0..2^36-1]
@@ -30,3 +30,40 @@ for l in input:
 for i in memory.values: sum += i
 
 echo "Part one: ", sum
+
+proc permutations(a: string): seq[string]=
+  var n = a.count('X')
+  if n == 0: return @[]
+  var perm = toseq(0..2^n-1).mapIt(toBin(it,n))
+  for p in perm.mitems():
+    var i = a
+    for j in i.mitems():
+      if j == 'X':
+        if len(p) > 0: j = p[0]
+        if len(p) > 1: p = p[1..^1]
+        else: p = ""
+    result.add(i)
+
+proc applyMask2(x: Bit, m: string): seq[Bit]=
+  var b = toBin(x,36)
+  for i in countup(0,m.len-1):
+    if m[i] == '0': continue
+    b[i] = m[i]
+  result = permutations(b).mapIt(Bit(parseBinInt(it)))
+
+mask = ""
+sum = 0
+memory = initTable[Bit,Bit]()
+for l in input:
+  if l[0..3] == "mask":
+    mask = l[7..^1]
+  else:
+    var
+      a = Bit(parseInt(l[4..l.find("]",5)-1]))
+      v = Bit(parseInt(l[l.find("=",0)+2..^1]))
+      m = applyMask2(a, mask)
+    for i in m: memory[i] = v
+
+for i in memory.values: sum += i
+
+echo "Part two: ", sum
